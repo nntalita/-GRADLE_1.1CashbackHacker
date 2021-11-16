f372aaa073eaa94a23f4882e50465390ed76ef39
Natalya Pletneva <nntalita@gmail.com>
2021-11-16 23:06:41 +0300

    Legacy

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,14 +1,17 @@
 package ru.netology.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
+
     @Test
-    public void shouldRemainLessThenBoundary() {
+    public void shouldRemainLessThenBoundaryJUnitVintage() {
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
@@ -16,7 +19,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldRemainMoreThenBoundary() {
+    public void shouldRemainMoreThenBoundaryJUnitVintage() {
         int amount = 1900;
         int actual = service.remain(amount);
         int expected = 100;
@@ -24,8 +27,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-
-    public void shouldRemainMultipleBoundary() {
+    public void shouldRemainMultipleBoundaryJUnitVintage() {
         int amount = 2000;
         int actual = service.remain(amount);
         int expected = 0;
@@ -33,10 +35,43 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldRemainZero() {
+    public void shouldRemainZeroJUnitVintage() {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
         assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldRemainLessThenBoundary() {
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldRemainMoreThenBoundary() {
+        int amount = 1900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+
+    public void shouldRemainMultipleBoundary() {
+        int amount = 2000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assertions.assertEquals (expected, actual);
+    }
+
+    @Test
+    public void shouldRemainZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        Assertions.assertEquals(expected, actual);
     }
 }
